dda6d69f42f751453039434771bb59f9bb062bab
anazz-dev <78837991+anazz-dev@users.noreply.github.com>
2023-03-17 17:41:23 +0100

    update script.js to end at 30 rounds

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const gameDiv = document.getElementById('game');
 const nextButton = document.getElementById('next');
 const balanceElement = document.getElementById('balance');
 const eventElement = document.getElementById('event');
+const resultElement = document.createElement('p');
 
 let balance = 1000;
+let round = 0;
+const maxRounds = 30;
 
 startButton.addEventListener('click', () => {
     startButton.hidden = true;
@@ -12,6 +15,7 @@ startButton.addEventListener('click', () => {
 });
 
 nextButton.addEventListener('click', () => {
+    round++;
     const randomEvent = Math.floor(Math.random() * 3) - 1;
     balance += randomEvent * 100;
     balanceElement.textContent = `Balance: $${balance}`;
@@ -22,5 +26,11 @@ nextButton.addEventListener('click', () => {
         eventElement.textContent = `Random Event: Gained $${randomEvent * 100}`;
     } else {
         eventElement.textContent = 'Random Event: No change';
+    }
+
+    if (round >= maxRounds) {
+        nextButton.disabled = true;
+        resultElement.textContent = "In the grand scheme of life, we arrive empty-handed and depart the same way.";
+        gameDiv.appendChild(resultElement);
     }
 });
