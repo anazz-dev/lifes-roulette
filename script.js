fed0197ed3d4144d209aa26d8ba4b9d793de20e8
anazz-dev <78837991+anazz-dev@users.noreply.github.com>
2023-03-18 16:29:50 +0100

    V2.0
    In this version, the game starting point is randomised, time series to visualise the relation between wealth (previously called balance) and age (previously called rounds).

--- script.js
+++ script.js
@@ -1,36 +1,87 @@
+const balanceChartCanvas = document.getElementById('balanceChart');
+
+let balanceChartData = {
+  labels: [],
+  datasets: [
+    {
+      label: 'Monetary Success Over the Years',
+      data: [],
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1,
+      pointRadius: 2,
+    },
+  ],
+};
+
+let balanceChart = new Chart(balanceChartCanvas, {
+  type: 'line',
+  data: balanceChartData,
+  options: {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Age',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Wealth',
+        },
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
 const startButton = document.getElementById('start');
 const gameDiv = document.getElementById('game');
 const nextButton = document.getElementById('next');
-const balanceElement = document.getElementById('balance');
+const wealthElement = document.getElementById('wealth');
 const eventElement = document.getElementById('event');
 const resultElement = document.createElement('p');
 
-let balance = 1000;
-let round = 0;
-const maxRounds = 30;
+let wealth = Math.floor(Math.random() * 1001) - 500;
+let age = 0;
+const maxAge = 75;
 
 startButton.addEventListener('click', () => {
-    startButton.hidden = true;
-    gameDiv.hidden = false;
+  startButton.hidden = true;
+  gameDiv.hidden = false;
+  wealthElement.textContent = `Wealth: $${wealth}`; // Add this line to show the starting wealth
 });
 
 nextButton.addEventListener('click', () => {
-    round++;
-    const randomEvent = Math.floor(Math.random() * 3) - 1;
-    balance += randomEvent * 100;
-    balanceElement.textContent = `Balance: $${balance}`;
+  age++;
+  const impactFactor = age <= 20 ? 1 : (75 - age) / 40;
+  const randomEvent = Math.floor(Math.random() * 3) - 1;
+  wealth += randomEvent * 100 * impactFactor;
+  wealthElement.textContent = `Wealth: $${Math.round(wealth)}`;
 
-    if (randomEvent < 0) {
-        eventElement.textContent = `Random Event: Lost $${-randomEvent * 100}`;
-    } else if (randomEvent > 0) {
-        eventElement.textContent = `Random Event: Gained $${randomEvent * 100}`;
-    } else {
-        eventElement.textContent = 'Random Event: No change';
-    }
+  if (randomEvent < 0) {
+    eventElement.textContent = `Random Event: Lost $${Math.round(-randomEvent * 100 * impactFactor)}`;
+  } else if (randomEvent > 0) {
+    eventElement.textContent = `Random Event: Gained $${Math.round(randomEvent  * 100 * impactFactor)}`;
+  } else {
+    eventElement.textContent = 'Random Event: No change';
+  }
 
-    if (round >= maxRounds) {
-        nextButton.disabled = true;
-        resultElement.textContent = "In the grand scheme of life, we arrive empty-handed and depart the same way.";
-        gameDiv.appendChild(resultElement);
-    }
+  // Late life event with a 10% chance of happening between ages 50 and 60
+  if (age >= 40 && age <= 60 && Math.random() < 0.1) {
+    const lateLifeEvent = Math.floor(Math.random() * 2001) - 1000;
+    wealth += lateLifeEvent;
+    wealthElement.textContent = `Wealth: $${Math.round(wealth)}`;
+    eventElement.textContent += ` | Late Life Event: ${(lateLifeEvent > 0 ? 'Gained' : 'Lost')} $${Math.abs(lateLifeEvent)}`;
+  }
+
+  balanceChartData.labels.push(age);
+  balanceChartData.datasets[0].data.push(wealth);
+  balanceChart.update();
+
+  if (age >= 75) {
+    nextButton.disabled = true;
+    resultElement.textContent = "In the grand scheme of life, we arrive empty-handed 👶🏼 and depart empty-handed.⚰️";
+    gameDiv.appendChild(resultElement);
+  }
 });
